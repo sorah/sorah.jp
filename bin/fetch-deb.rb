1a7eb0228fb21bf4f494e6f16a4ef14df8cdecf5
Sorah Fukumori <her@sorah.jp>
2020-04-26 02:43:44 +0900

    debian packages for Ubuntu 20.04 focal

--- bin/fetch-deb.rb
+++ bin/fetch-deb.rb
@@ -9,6 +9,7 @@ repos = {
     xenial: 'https://cache.ruby-lang.org/lab/sorah/deb/dists/xenial/main/binary-amd64/Packages',
     trusty: 'https://cache.ruby-lang.org/lab/sorah/deb/dists/trusty/main/binary-amd64/Packages',
     bionic: 'https://cache.ruby-lang.org/lab/sorah/deb/dists/bionic/main/binary-amd64/Packages',
+    focal: 'https://cache.ruby-lang.org/lab/sorah/deb/dists/focal/main/binary-amd64/Packages',
     stretch: 'https://cache.ruby-lang.org/lab/sorah/deb/dists/stretch/main/binary-amd64/Packages',
     buster: 'https://cache.ruby-lang.org/lab/sorah/deb/dists/buster/main/binary-amd64/Packages',
   },
@@ -16,19 +17,23 @@ repos = {
     xenial: 'https://deb.nekom.it/dists/xenial/main/binary-amd64/Packages',
     trusty: 'https://deb.nekom.it/dists/trusty/main/binary-amd64/Packages',
     bionic: 'https://deb.nekom.it/dists/bionic/main/binary-amd64/Packages',
+    focal: 'https://deb.nekom.it/dists/focal/main/binary-amd64/Packages',
+    stretch: 'https://deb.nekom.it/dists/stretch/main/binary-amd64/Packages',
+    buster: 'https://deb.nekom.it/dists/buster/main/binary-amd64/Packages',
   }
 }
 
 repos = repos.map do |repo, dists|
   [repo, dists.map do |dist, url|
     # source->package->versions
-    debs = open(url, 'r', &:read)
+    debs = URI.open(url, 'r', &:read)
       .each_line
       .slice_after{ |_| _.chomp.empty? }
       .map { |chunk| chunk.map(&:chomp).reject(&:empty?).slice_before { |_| _[0] != ' ' }.map { |_| key, val = _.first.split(': ', 2); [key.sub(/: ?\z/,''), [val.nil? || val.empty? ? nil : val, *_[1..-1].map{ |_| _.sub(/^ /,'') }].compact] }.to_h }
       .group_by{ |_| (_['Source'] || _['Package'])[0] }
       .map{ |source, versions| [source, versions.group_by{ |_| _['Package'][0] }] }
       .to_h
+    p debs
 
     [dist,
       debs.map do |source, packages|
